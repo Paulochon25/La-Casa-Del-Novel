24f0a6beaeee41f3133453fd43b1728361d6e6b5
Your Name <you@example.com>
2021-06-21 15:18:29 +0200

    init

---
+++ src/Controller/NovelController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NovelController extends AbstractController
+{
+    /**
+     * @Route("/LN", name="LN")
+     */
+    public function listLn(): Response
+    {
+        return $this->render('list_ln.html.twig');
+    }
+    /**
+     * @Route("/traducteurs", name="translators")
+     */
+    public function translators(): Response
+    {
+        return $this->render('translators.html.twig');
+    }
+    /**
+     * @Route("/forum", name="forum")
+     */
+    public function forum(): Response
+    {
+        return $this->render('forum.html.twig');
+    }
+}
