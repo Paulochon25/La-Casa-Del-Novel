eadbc31680ce0d153e906469513b098745821a1f
Your Name <you@example.com>
2021-06-26 00:06:53 +0200

    initial commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function Home(): Response
+    {
+        return $this->render('index/index.html.twig');
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function Register(): Response
+    {
+
+    }
+}
